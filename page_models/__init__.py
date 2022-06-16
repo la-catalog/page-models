4bf63aba5e8fb25a1e191e4445083e4b976a3826
thiagola92 <thiagola92@gmail.com>
2022-06-16 08:09:53 -0300

    Add shortcut in init

--- page_models/__init__.py
+++ page_models/__init__.py
@@ -1,3 +1,4 @@
+from page_models.query.query import Query
 from page_models.sku.attribute import Attribute
 from page_models.sku.measurement import Measurement
 from page_models.sku.metadata import Metadata
