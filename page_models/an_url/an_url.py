3ea932812d5631cc698354cb466bf997e229e3f8
thiagola92 <thiagola92@gmail.com>
2022-08-25 05:36:14 -0300

    Add docstring to AnURL

--- page_models/an_url/an_url.py
+++ page_models/an_url/an_url.py
@@ -2,6 +2,18 @@ from pydantic import AnyHttpUrl, BaseModel, validator
 
 
 class AnURL(BaseModel):
+    """
+    All pydantic URL objects require you to give the scheme when creating
+    directly from them:
+        AnyHttpUrl(url="https://www.example.com", scheme="https")
+        AnyUrl(url="https://www.example.com", scheme="https")
+        HttpUrl(url="https://www.example.com", scheme="https")
+    But don't require while validating the type.
+
+    This model exists so you can do:
+        AnURL(url="https://www.example.com")
+    """
+
     url: AnyHttpUrl
 
     _url = validator("url", allow_reuse=True)(lambda u: str(u))
