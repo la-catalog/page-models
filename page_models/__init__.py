0f2c57cc5be2506ba895673f7bfa94d403efd875
thiagola92 <thiagola92@gmail.com>
2022-11-09 06:05:50 -0300

    Remove Metadata from __init__.py to avoid confusion between SKU and Query metadatas

--- page_models/__init__.py
+++ page_models/__init__.py
@@ -3,7 +3,6 @@ from page_models.sku.attribute import Attribute
 from page_models.sku.audio import Audio
 from page_models.sku.image import Image
 from page_models.sku.measurement import Measurement
-from page_models.sku.metadata import Metadata
 from page_models.sku.price import Price
 from page_models.sku.rating import Rating
 from page_models.sku.sku import SKU
