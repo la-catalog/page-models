908b634b7d90348d311245077ee054f589db5f1b
thiagola92 <thiagola92@gmail.com>
2022-11-09 06:08:34 -0300

    Add origin to Metadata about query

--- page_models/query/metadata.py
+++ page_models/query/metadata.py
@@ -9,8 +9,14 @@ from page_models.core import CoreModel, core_config
 @dataclass(config=core_config)
 class Metadata(CoreModel):
     """
+    Metadata about Query.
+
     created - When the query was created
+
+    origin - Who triggered the pipeline
     """
 
     # Datetime fields (UTC time)
     created: datetime = Field(default_factory=datetime.utcnow)
+
+    origin: str = None
